e873cdd72f9c10227d89d143712e1633f1db0ada
xieqiankun <xieqk@zenme.com>
2018-03-29 17:57:51 +0800

    update two submission

---
+++ binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,62 @@
+"""
+https://leetcode.com/submissions/detail/147517696/
+
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+
+
+return its bottom-up level order traversal as:
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        def get_leaf(node_list, result):
+            new_node_list = []
+            value_list = []
+            if not any(node_list):
+                return
+            for node in node_list:
+                if node:
+                    value_list.append(node.val)
+                    new_node_list.append(node.left)
+                    new_node_list.append(node.right)
+            result.append(value_list)
+            get_leaf(new_node_list, result)
+
+        if not root:
+            return []
+        
+        result = []
+        get_leaf([root], result)
+        return result[::-1]
+        
+        
---
+++ convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,61 @@
+"""
+https://leetcode.com/submissions/detail/147528781/
+
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+
+
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        def insert(node, val_list):
+            if not val_list:
+                return
+
+            middle_index = len(val_list) // 2
+            middle = val_list[middle_index]
+            if middle < node.val:
+                node.left = TreeNode(middle)
+                insert(node.left, val_list[:middle_index])
+                insert(node.left, val_list[middle_index + 1: ])
+            else:
+                node.right = TreeNode(middle)
+                insert(node.right, val_list[middle_index + 1: ])
+                insert(node.right, val_list[: middle_index])
+
+        if not nums:
+            return None
+        middle_index = len(nums) // 2
+        root = TreeNode(nums[middle_index])
+        insert(root, nums[:middle_index])
+        insert(root, nums[middle_index + 1:])
+        return root
+        
