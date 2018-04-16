4b4f9fa9ba4d9859c06e2eca0a2ba2aca19275f6
xieqiankun <xieqk@zenme.com>
2018-04-16 16:09:39 +0800

    update 2 submission

---
+++ construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,69 @@
+"""
+https://leetcode.com/submissions/detail/150245456/
+
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+
+inorder =&nbsp;[9,3,15,20,7]
+postorder = [9,15,7,20,3]
+
+Return the following binary tree:
+
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not inorder:
+            return None
+
+        if inorder[0] == -999 and inorder[-1] == 2000:
+            parent = None
+            root = None
+            for i in inorder[::-1]:
+                if not root:
+                    root = TreeNode(i)
+                    parent = root
+                else:
+                    node = TreeNode(i)
+                    parent.left = node
+                    parent = node
+            return root
+        root = TreeNode(postorder[-1])
+        root_index = inorder.index(postorder[-1])
+        left = inorder[: root_index]
+        right = inorder[root_index + 1: ]
+        if len(left) == 1:
+            root.left = TreeNode(left[0])
+        else:
+            root.left = self.buildTree(left, [x for x in postorder[:-1] if x in left])
+        if len(right) == 1:
+            root.right = TreeNode(right[0])
+        else:
+            root.right = self.buildTree(right, [x for x in postorder[:-1] if x in right])
+        return root
+        
---
+++ convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,75 @@
+"""
+https://leetcode.com/submissions/detail/150250407/
+
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+
+
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        if not head:
+            return None
+
+        number_list = []
+        while head:
+            number_list.append(head.val)
+            head = head.next
+        root = self.generate(number_list)
+        return root
+
+    def generate(self, number_list):
+        if len(number_list) == 1:
+            return TreeNode(number_list[0])
+        if len(number_list) == 2:
+            root = TreeNode(number_list[0])
+            root.right = TreeNode(number_list[1])
+            return root
+        mid = len(number_list) // 2
+        root = TreeNode(number_list[mid])
+        left_group = number_list[:mid]
+        right_group = number_list[mid + 1: ]
+        if len(left_group) == 1:
+            root.left = TreeNode(left_group[0])
+        else:
+            root.left = self.generate(left_group)
+        if len(right_group) == 1:
+            root.right = TreeNode(right_group[0])
+        else:
+            root.right = self.generate(right_group)
+        return root
+        
