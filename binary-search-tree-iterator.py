b07f3ba0f63f85378e7436dfd381dd263beaacc4
kingname <greensouth@foxmail.com>
2020-07-24 21:33:22 +0800

    new solution

---
+++ binary-search-tree-iterator.py
@@ -0,0 +1,98 @@
+"""
+https://leetcode.com/submissions/detail/370986912/
+
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+&nbsp;
+
+
+
+
+Example:
+
+
+
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // return 3
+iterator.next();    // return 7
+iterator.hasNext(); // return true
+iterator.next();    // return 9
+iterator.hasNext(); // return true
+iterator.next();    // return 15
+iterator.hasNext(); // return true
+iterator.next();    // return 20
+iterator.hasNext(); // return false
+
+
+&nbsp;
+
+Note:
+
+
+	next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+	You may assume that&nbsp;next()&nbsp;call&nbsp;will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        from collections import deque
+        self.root = root
+        self.stop = False
+        self.iterator = self.do_iter(self.root)
+        self.next_val = deque()
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        if self.next_val:
+            return self.next_val.popleft()
+        
+        return self.try_to_get_next_val()
+
+    def do_iter(self, node):
+        if not node:
+            return
+        if node.left:
+            yield from self.do_iter(node.left)
+        yield node.val
+        if node.right:
+            yield from self.do_iter(node.right)
+        
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        next_val = self.try_to_get_next_val()
+        if next_val is not None:
+            self.next_val.append(next_val)
+            return True
+        else:
+            return False
+
+    def try_to_get_next_val(self):
+        try:
+            value = next(self.iterator)
+            return value
+        except StopIteration:
+            return None
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
---
+++ sort-list.py
@@ -0,0 +1,70 @@
+"""
+https://leetcode.com/submissions/detail/369690505/
+
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+
+
+Input: 4-&gt;2-&gt;1-&gt;3
+Output: 1-&gt;2-&gt;3-&gt;4
+
+
+Example 2:
+
+
+Input: -1-&gt;5-&gt;3-&gt;4-&gt;0
+Output: -1-&gt;0-&gt;3-&gt;4-&gt;5
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def merge(self, node1, node2):
+        temp_node = temp_node_head = ListNode(0)
+        while node1 and node2:
+            if node1.val < node2.val:
+                temp_node_head.next = node1
+                node1 = node1.next
+            else:
+                temp_node_head.next = node2
+                node2 = node2.next
+            temp_node_head = temp_node_head.next
+        temp_node_head.next = node1 or node2
+        temp_node = temp_node.next
+        return temp_node
+            
+
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        if not head.next:
+            return head
+        
+        slow = head
+        fast = head
+        
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        node_list_2_head = slow.next
+        slow.next = None
+        left = self.sortList(head)
+        right = self.sortList(node_list_2_head)
+        return self.merge(left, right)
+
+        
+        
+            
+        
+                        
+                
+        
+            
+            
